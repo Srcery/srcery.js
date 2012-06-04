8dce7727f2c85045d3d393d41badd7fc25b787f7
Travis Tidwell <travist349@gmail.com>
2012-06-03 21:32:56 -0500

    Fixed it so that images work with javascript turned off.

--- assets/srcery.js/src/srcery.image.js
+++ assets/srcery.js/src/srcery.image.js
@@ -1,5 +1,33 @@
 srcery.image = function() {
 };
+
 srcery.onReady(function() {
-  console.log('image component ready');
+  jQuery('.srcery-img').each(function() {
+    var src = jQuery(this).attr('src');
+    jQuery(this).filedrop({
+      paramname:'pic',
+      maxfiles: 5,
+      maxfilesize: 2,
+      url: src,
+      uploadFinished:function(i,file,response){
+        console.log('finished!');
+        console.log(response);
+      },
+      error: function(err, file) {
+        console.log(err);
+      },
+      beforeEach: function(file){
+        if(!file.type.match(/^image\//)){
+          alert('Only images are allowed!');
+          return false;
+        }
+      },
+      uploadStarted:function(i, file, len){
+        console.log(file);
+      },
+      progressUpdated: function(i, file, progress) {
+        console.log('Uploading...' + progress);
+      }
+    });
+  });
 });
